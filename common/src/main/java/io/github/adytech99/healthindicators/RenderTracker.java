4030afbe2101baa34bb7016147f56dd405ca19a6
SomebodyDoingThings <296626985+SomebodyDoingThings@users.noreply.github.com>
2026-09-15 18:45:30 -0500

    fix RenderTracker.java
    i'm stupid oops

--- common/src/main/java/io/github/adytech99/healthindicators/RenderTracker.java
+++ common/src/main/java/io/github/adytech99/healthindicators/RenderTracker.java
@@ -185,7 +185,7 @@ public class RenderTracker {
                 || entity.touchingUnloadedChunk()
                 || !(entity instanceof LivingEntity)
                 || client.player == null
-                || client.player.getVehicle() == entity
+                || client.player.getVehicle() == entity);
     }
     private static Entity getEntityFromUUID(UUID uuid, ClientLevel world) {
         for (Entity entity : world.entitiesForRendering()) {
